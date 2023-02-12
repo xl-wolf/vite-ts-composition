c3075ffcadc74cca315aacb4481adc0d568b4ba5
pc <758087477@qq.com>
2023-02-12 15:16:37 +0800

    需求开发

--- src/views/form/upload.vue
+++ src/views/form/upload.vue
@@ -9,9 +9,16 @@
     <div class="button-group">
       <el-button type="primary" @click="uploadAction('收款单')">上传收款单</el-button>
       <el-button type="primary" @click="uploadAction('付款单')">上传付款单</el-button>
-      <el-button type="primary" @click="uploadAction('售后仓库退款单')">上传售后仓库退款单</el-button>
-      <el-button type="primary" @click="uploadAction('售后渠道退款单')">上传售后渠道退款单</el-button>
+      <el-button type="primary" @click="uploadAction('售后单')">上传售后单</el-button>
       <el-button type="primary" @click="uploadAction('源数据')">上传源数据</el-button>
+    </div>
+
+    <div class="success-list">
+        已上传成功文件记录：
+        <div v-for="(file,index) in uploadedFileList">
+            {{`${index+1}、${file.name}`}}
+            <el-button type="danger" size="small" @click="delAction(file)">删除</el-button>
+        </div>
     </div>
   </div>
 </template>
@@ -40,15 +47,28 @@ const uploadAction = (type: string) => {
   uploadRef.value!.submit()
 }
 
+const uploadedFileList = ref([])
 // 上传前校验
 const beforeUpload = (file: File) => {
   console.log(file)
   const name = file.name;
+  if(uploadedFileList.value.some((tempfile:File)=>tempfile.name === file.name)){
+     ElMessage.warning({ message: `${file.name}已上传成功过！` });
+     return false
+  }
+  uploadedFileList.value.push(file)
+  console.log(uploadedFileList.value,'uploadedFileList.vlaue')
+
   if (!name.includes('xlsx')) {
     ElMessage.warning({ message: '目前只支持上传xlsx后缀名的excel文件' });
     return false
   }
 };
+
+// 删除方法
+const delAction = (file: File)=>{
+    console.log(file)
+}
 
 </script>
 
@@ -68,4 +88,9 @@ const beforeUpload = (file: File) => {
 .button-group{
   margin-top: 50px;
 }
+
+.success-list{
+    margin-top: 20px;
+}
+
 </style>
