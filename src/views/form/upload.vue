6573a1ec51fe43952ed24d6a6fe59d9593490090
xl-wolf <1072236984@qq.com>
2023-02-13 21:24:03 +0800

    实时可视excel，table功能开发

--- src/views/form/upload.vue
+++ src/views/form/upload.vue
@@ -1,74 +1,145 @@
 <template>
   <div class="container">
     <div class="content-title">支持拖拽</div>
-    <el-upload class="upload-com" ref="uploadRef" :before-upload="beforeUpload" :http-request="handleUpload" drag
+    <el-upload class="upload-com" ref="uploadRef" v-model:file-list="fileList" :on-change="handleChange" drag
       :auto-upload="false" multiple>
       <el-icon class="el-icon--upload"><upload-filled /></el-icon>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
     </el-upload>
-    <div class="button-group">
-      <el-button type="primary" @click="uploadAction('收款单')">上传收款单</el-button>
-      <el-button type="primary" @click="uploadAction('付款单')">上传付款单</el-button>
-      <el-button type="primary" @click="uploadAction('售后单')">上传售后单</el-button>
-      <el-button type="primary" @click="uploadAction('源数据')">上传源数据</el-button>
-    </div>
 
-    <div class="success-list">
-        已上传成功文件记录：
-        <div v-for="(file,index) in uploadedFileList">
-            {{`${index+1}、${file.name}`}}
-            <el-button type="danger" size="small" @click="delAction(file)">删除</el-button>
-        </div>
-    </div>
+    <div class="divider" />
+    <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+      <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+      <el-table-column prop="name" label="用户名"></el-table-column>
+      <el-table-column label="账户余额">
+        <template #default="scope">￥{{ scope.row.money }}</template>
+      </el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column label="状态" align="center">
+        <template #default="scope">
+          <el-tag :type="scope.row.state === '成功' ? 'success' : scope.row.state === '失败' ? 'danger' : ''">
+            {{ scope.row.state }}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="date" label="注册时间"></el-table-column>
+      <el-table-column label="操作" width="220" align="center" v-if="0 && canOperate">
+        <template #default="scope">
+          <el-button text :icon="Edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button text :icon="Delete" class="red" @click="handleDelete(scope.$index)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="divider" />
+    <el-button type="primary" @click="exportXlsx">导出Excel</el-button>
   </div>
+
+
 </template>
 
 <script setup lang="ts">
-import { uploadApi } from '../../api/upload';
-import { ref } from 'vue'
-import { ElMessage, UploadInstance } from 'element-plus'
+import { computed, ref } from 'vue'
+import { ElButton, ElIcon, ElMessage, ElTable, ElTableColumn, ElTag, ElUpload, UploadFile, UploadProps, UploadUserFile } from 'element-plus'
+import { UploadFilled, Edit, Delete } from '@element-plus/icons-vue';
+import { usePermissStore } from '../../store/permiss';
+import * as XLSX from 'xlsx';
 
-const handleUpload = (params: any) => {
-  const formData = new FormData()
-  formData.append('file', params.file)
-  formData.append('fileType', fileType.value)
-  uploadApi(formData).then(res => {
-    if (res) {
-      ElMessage.success({ message: '上传成功' });
-      uploadRef.value!.clearFiles()
-    }
-  })
-};
 
-const uploadRef = ref<UploadInstance>()
-const fileType = ref('')
-const uploadAction = (type: string) => {
-  fileType.value = type
-  uploadRef.value!.submit()
-}
-
-const uploadedFileList = ref([])
-// 上传前校验
-const beforeUpload = (file: File) => {
-  console.log(file)
-  const name = file.name;
-  if(uploadedFileList.value.some((tempfile:File)=>tempfile.name === file.name)){
-     ElMessage.warning({ message: `${file.name}已上传成功过！` });
-     return false
+const fileList = ref<UploadFile[]>([])
+const handleChange: UploadProps['onChange'] = (uploadFile, uploadFiles) => {
+  console.log(fileList.value, 'fileList.vlaue', uploadFiles)
+  const name = uploadFile.name;
+  if (fileList.value.some((tempfile: UploadFile) => tempfile.name === uploadFile.name)) {
+    ElMessage.warning({ message: `${uploadFile.name}已上传过！` });
+    return false
   }
-  uploadedFileList.value.push(file)
-  console.log(uploadedFileList.value,'uploadedFileList.vlaue')
+  fileList.value.push(uploadFile)
+  console.log(fileList.value, 'fileList.vlaue', uploadFiles)
 
   if (!name.includes('xlsx')) {
     ElMessage.warning({ message: '目前只支持上传xlsx后缀名的excel文件' });
     return false
   }
+}
+
+const list = [['序号', '姓名', '学号', '班级', '年龄', '性别']];
+const exportXlsx = () => {
+  tableData.value.map((item: any, i: number) => {
+    const arr: any[] = [i + 1];
+    arr.push(...[item.name, item.sno, item.class, item.age, item.sex]);
+    list.push(arr);
+  });
+  let WorkSheet = XLSX.utils.aoa_to_sheet(list);
+  let new_workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(new_workbook, WorkSheet, '第一页');
+  XLSX.writeFile(new_workbook, `表格.xlsx`);
 };
 
-// 删除方法
-const delAction = (file: File)=>{
-    console.log(file)
+const permiss = usePermissStore()
+const canOperate = computed(() => JSON.stringify(permiss.key) === JSON.stringify(permiss.defaultList.admin))
+
+const handleEdit = (index: number, row: any) => {
+  console.log(index, row)
+};
+const handleDelete = (index: number) => {
+  console.log(index)
+};
+
+interface TableItem {
+  id: number;
+  name: string;
+  money: number;
+  state: string;
+  date: string;
+  address: string;
 }
+const tableData = ref<TableItem[]>([]);
+const pageTotal = ref(0);
+// 获取表格数据
+const getData = () => {
+  const res = {
+    list: [{
+      "id": 1,
+      "name": "张三",
+      "money": 123,
+      "address": "广东省东莞市长安镇",
+      "state": "成功",
+      "date": "2019-11-1",
+
+    },
+    {
+      "id": 2,
+      "name": "李四",
+      "money": 456,
+      "address": "广东省广州市白云区",
+      "state": "成功",
+      "date": "2019-10-11",
+    },
+    {
+      "id": 3,
+      "name": "王五",
+      "money": 789,
+      "address": "湖南省长沙市",
+      "state": "失败",
+      "date": "2019-11-11",
+    },
+    {
+      "id": 4,
+      "name": "赵六",
+      "money": 1011,
+      "address": "福建省厦门市鼓浪屿",
+      "state": "成功",
+      "date": "2019-10-20",
+    }],
+    pageTotal: 4
+  }
+  tableData.value = res.list;
+  pageTotal.value = res.pageTotal || 50;
+
+};
+getData();
 
 </script>
 
@@ -85,12 +156,11 @@ const delAction = (file: File)=>{
   width: 360px;
 }
 
-.button-group{
-  margin-top: 50px;
+.divider {
+  margin-top: 20px;
 }
 
-.success-list{
-    margin-top: 20px;
+.red {
+  color: #F56C6C;
 }
-
 </style>
--- src/views/table/import.vue
+++ src/views/table/import.vue
@@ -3,60 +3,71 @@
     <div class="container">
       <div class="handle-box">
         <el-upload action="#" :limit="1" accept=".xlsx, .xls" :show-file-list="false" :before-upload="beforeUpload"
-          :http-request="handleMany">
+          :http-request="httpRequest" ref='uploadRef' :on-success="handleSuccess">
           <el-button class="mr10" type="success">批量导入</el-button>
         </el-upload>
-        <el-link href="/template.xlsx" target="_blank">下载模板</el-link>
+        <el-button type="primary" @click="exportXlsx">导出Excel</el-button>
       </div>
-      <el-table :data="tableData" border class="table" header-cell-class-name="table-header">
-        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="sno" label="学号"></el-table-column>
-        <el-table-column prop="class" label="班级"></el-table-column>
-        <el-table-column prop="age" label="年龄"></el-table-column>
-        <el-table-column prop="sex" label="性别"></el-table-column>
+      <el-table :data="tableData" border stripe class="table" show-overflow-tooltip height="600">
+        <el-table-column :prop="column.prop" :label="column.label" v-for="column in columnFiledList"
+          :fixed="column.prop === 'orderNO' || column.prop === 'number' ? 'left' : column.prop === 'profit' ? 'right' : false"></el-table-column>
       </el-table>
     </div>
   </div>
 </template>
 
 <script setup lang="ts" name="import">
-import { UploadProps } from 'element-plus';
+import { ElMessage, UploadInstance, UploadProps } from 'element-plus';
 import { ref } from 'vue';
 import * as XLSX from 'xlsx';
 
+const columnFiledList: { [key: string]: string }[] = [
+  { prop: 'number', label: '序号' },
+  { prop: 'orderNO', label: '订单编号' },
+  { prop: 'name', label: '商品名称' },
+  { prop: 'count', label: '商品数量' },
+  { prop: 'canal', label: '渠道' },
+  { prop: 'price', label: '成本单价' },
+  { prop: 'cost', label: '成本（成本价X数量+供应商运费）' },
+  { prop: 'canalcost', label: '渠道价格' },
+  { prop: 'salefee', label: '销售金额（供货价X数量+运费）' },
+  { prop: 'buytime', label: '下单时间' },
+  { prop: 'receiver', label: '收件人' },
+  { prop: 'receiverphone', label: '收件人电话' },
+  { prop: 'receiveraddress', label: '收件人地址' },
+  { prop: 'productstatus', label: '发货状态' },
+  { prop: 'orderstatus', label: '订单状态' },
+  { prop: 'shouldpayfee', label: '应付金额' },
+  { prop: 'shouldgetfee', label: '应收金额' },
+  { prop: 'salecanalrefund', label: '售后渠道退款金额' },
+  { prop: 'salerepositoryrefund', label: '售后仓库退款金额' },
+  { prop: 'profit', label: '利润' },
+]
+
 interface TableItem {
-  id: number;
-  name: string;
-  sno: string;
-  class: string;
-  age: string;
-  sex: string;
+  number: number,
+  orderNO: string
+  name: string
+  count: string
+  canal: string
+  price: string
+  cost: string
+  canalcost: string
+  salefee: string
+  buytime: string
+  receiver: string
+  receiverphone: string
+  receiveraddress: string
+  productstatus: string
+  orderstatus: string
+  shouldpayfee: string
+  shouldgetfee: string
+  salecanalrefund: string
+  salerepositoryrefund: string
+  profit: string,
 }
 
 const tableData = ref<TableItem[]>([]);
-// 获取表格数据
-const getData = () => {
-  tableData.value = [
-    {
-      id: 1,
-      name: '小明',
-      sno: 'S001',
-      class: '一班',
-      age: '10',
-      sex: '男',
-    },
-    {
-      id: 2,
-      name: '小红',
-      sno: 'S002',
-      class: '一班',
-      age: '9',
-      sex: '女',
-    },
-  ];
-};
-getData();
 
 const importList = ref<any>([]);
 const beforeUpload: UploadProps['beforeUpload'] = async (rawFile) => {
@@ -68,9 +79,8 @@ const analysisExcel = (file: any) => {
     const reader = new FileReader();
     reader.onload = function (e: any) {
       const data = e.target.result;
-      let datajson = XLSX.read(data, {
-        type: 'binary',
-      });
+      const datajson = XLSX.read(data, { type: 'binary' });
+      console.log(datajson)
 
       const sheetName = datajson.SheetNames[0];
       const result = XLSX.utils.sheet_to_json(datajson.Sheets[sheetName]);
@@ -80,23 +90,164 @@ const analysisExcel = (file: any) => {
   });
 };
 
-const handleMany = async () => {
+const httpRequest = async () => {
   // 把数据传给服务器后获取最新列表，这里只是示例，不做请求
+  let ordertype = ''
   const list = importList.value.map((item: any, index: number) => {
-    return {
-      id: index,
-      name: item['姓名'],
-      sno: item['学号'],
-      class: item['班级'],
-      age: item['年龄'],
-      sex: item['性别'],
-    };
+    if (item['应付金额'] === 0 || item['应付金额']) {
+      ordertype = '付款单'
+      return {
+        orderNO: item['订单编号'],
+        shouldpayfee: item['应付金额'],
+      }
+    }
+    else if (item['应收金额'] === 0 || (item['应收金额'])) {
+      ordertype = '收款单'
+      return {
+        orderNO: item['订单编号'],
+        shouldgetfee: item['应收金额'],
+      }
+    }
+    else if ((item['售后渠道退款金额'] === 0 || item['售后渠道退款金额']) && (item['售后仓库退款金额'] === 0 || item['售后仓库退款金额'])) {
+      ordertype = '售后单'
+      return {
+        orderNO: item['订单编号'],
+        salecanalrefund: item['售后渠道退款金额'],
+        salerepositoryrefund: item['售后仓库退款金额'],
+      }
+    }
+    else {
+      ordertype = '源数据'
+      return {
+        number: index + 1,
+        orderNO: item['订单编号'],
+        name: item['商品名称'],
+        count: item['商品数量'],
+        canal: item['渠道'],
+        price: item['成本单价'],
+        cost: item['成本（成本价X数量+供应商运费）'],
+        canalcost: item['渠道价格'],
+        salefee: item['销售金额（供货价X数量+运费）'],
+        buytime: item['下单时间'],
+        receiver: item['收件人'],
+        receiverphone: item['收件人电话'],
+        receiveraddress: item['收件人地址'],
+        productstatus: item['发货状态'],
+        orderstatus: item['订单状态'],
+        shouldpayfee: item['应付金额'],
+        shouldgetfee: item['应收金额'],
+        salecanalrefund: item['售后渠道退款金额'],
+        salerepositoryrefund: item['售后仓库退款金额'],
+        profit: item['应收金额'] - item['应付金额'] - item['售后渠道退款金额'] + item['售后仓库退款金额'],
+      };
+    }
   });
-  tableData.value.push(...list);
+  console.log(ordertype + ':', list)
+  const tmp: { [key: string]: any } = {}
+  list.forEach((item: any) => {
+    if (!item.orderNO) ElMessage.warning('存在没有订单编号的数据')
+
+    if (item.shouldpayfee === 0 || item.shouldpayfee) {
+      if (!tmp[item.orderNO]) {
+        tmp[item.orderNO] = item.shouldpayfee
+      } else {
+        tmp[item.orderNO] = [tmp[item.orderNO]].concat(item.shouldpayfee)
+        ElMessage.warning(`订单编号为${item.orderNO}的数据存在重复`)
+      }
+    }
+    else if (item.shouldgetfee === 0 || (item.shouldgetfee)) {
+      if (!tmp[item.orderNO]) {
+        tmp[item.orderNO] = item.shouldgetfee
+      } else {
+        tmp[item.orderNO] = [tmp[item.orderNO]].concat(item.shouldgetfee)
+        ElMessage.warning(`订单编号为${item.orderNO}的数据存在重复`)
+      }
+    }
+    else if ((item.salecanalrefund === 0 || item.salecanalrefund) && (item.salerepositoryrefund === 0 || item.salerepositoryrefund)) {
+      if (!tmp[item.orderNO]) {
+        tmp[item.orderNO] = `售后渠道退款金额:${item.salecanalrefund}售后仓库退款金额:${item.salerepositoryrefund}`
+      } else {
+        tmp[item.orderNO] = [tmp[item.orderNO]].concat(`售后渠道退款金额:${item.salecanalrefund}售后仓库退款金额:${item.salerepositoryrefund}`)
+        ElMessage.warning(`订单编号为${item.orderNO}的数据存在重复`)
+      }
+    }
+    else {
+      if (!tmp[item.orderNO]) {
+        tmp[item.orderNO] = item
+      } else {
+        tmp[item.orderNO] = [tmp[item.orderNO]].concat(item)
+        ElMessage.warning(`订单编号为${item.orderNO}的数据存在重复`)
+      }
+    }
+  });
+  console.log(tmp, 'tmp')
+  // tableData.value.push(...list);
+  // console.log(tableData.value)
+};
+
+const uploadRef = ref<UploadInstance>()
+const handleSuccess = () => uploadRef.value!.clearFiles(); //上传成功之后清除历史记录
+
+
+const list = [
+  [
+    '序号',
+    '订单编号',
+    '商品名称',
+    '商品数量',
+    '渠道',
+    '成本单价',
+    '成本（成本价X数量+供应商运费）',
+    '渠道价格',
+    '销售金额（供货价X数量+运费）',
+    '下单时间',
+    '收件人',
+    '收件人电话',
+    '收件人地址',
+    '发货状态',
+    '订单状态',
+    '应付金额',
+    '应收金额',
+    '售后渠道退款金额',
+    '售后仓库退款金额',
+    '利润',
+  ]
+];
+const exportXlsx = () => {
+  tableData.value.map((item: TableItem, i: number) => {
+    const arr: any[] = [];
+    arr.push(...[
+      item.number,
+      item.orderNO,
+      item.name,
+      item.count,
+      item.canal,
+      item.price,
+      item.cost,
+      item.canalcost,
+      item.salefee,
+      item.buytime,
+      item.receiver,
+      item.receiverphone,
+      item.receiveraddress,
+      item.productstatus,
+      item.orderstatus,
+      item.shouldpayfee,
+      item.shouldgetfee,
+      item.salecanalrefund,
+      item.salerepositoryrefund,
+      item.profit,
+    ]);
+    list.push(arr);
+  });
+  const WorkSheet = XLSX.utils.aoa_to_sheet(list);
+  const new_workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(new_workbook, WorkSheet, '第一页');
+  XLSX.writeFile(new_workbook, `表格.xlsx`);
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .handle-box {
   display: flex;
   margin-bottom: 20px;
@@ -106,6 +257,13 @@ const handleMany = async () => {
   width: 100%;
   font-size: 14px;
 }
+
+// /deep/.el-table .cell {
+//   max-width: 240px !important;
+//   white-space: nowrap !important;
+//   overflow: hidden !important;
+//   text-overflow: ellipsis !important;
+// }
 
 .mr10 {
   margin-right: 10px;
